df007718621c59006fce5b9f39f25dbc872c3e05
Mukesh Jaat <120589453+mukesh2465@users.noreply.github.com>
2025-06-28 01:24:40 +0530

    Time: 349 ms (52.86%) | Memory: 175.8 MB (34.29%) - LeetSync

--- 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
@@ -1,34 +1,64 @@
 class Solution {
 public:
-    string ans = "";
-    bool isSubsequence(string &s, string &temp) {
-        int i = 0;
-        for(auto &ch : s) {
-            if(i < temp.size() && ch == temp[i]) ++i;
-        }
-        return i == temp.size();
-    }
-    bool check(string &s, string &temp, int k) {
-        string repeated = "";
-        for(int i = 0; i < k; ++i) repeated += temp;
-        return isSubsequence(s, repeated);
-    }
-    void solve(string &s, string &temp, int k) {
-        for(auto ch = 'z'; ch >= 'a'; --ch) {
-            temp.push_back(ch);
-            if(check(s, temp, k)) {
-                // pick lexicographically greatest
-                if(temp.size() > ans.size() ||
-                (temp.size() == ans.size() && temp > ans)) ans = temp;
-
-                solve(s, temp, k);
-            }
-            temp.pop_back(); // backtrack
-        }
-    }
     string longestSubsequenceRepeatedK(string s, int k) {
-        string temp = "";
-        solve(s, temp, k);
+        unordered_map<int, int> freq;
+        // Count frequency of each character
+        for (auto ch: s){
+            freq[ch]++;
+        }
+        set<char, greater<char>> candidates;    //We needed lexicographically largest string, and this is taken care by this step
+        // Keep characters with freq ≥ k
+        for (auto [key, val]: freq){
+            if (val>=k) candidates.insert(key);
+        }
+
+        if (candidates.size()==0) return "";
+
+        queue<string> q;
+        // Start BFS with single-character strings
+        for (auto ch: candidates){
+            q.push(string(1, ch));  // string(1, ch) converts a character into a string of len 1
+        }
+
+        // //THIS POINT ONWARDS WE COULD HAVE USED A MODIFIED string s WHICH CONTAINS ONLY 
+        // //THE CHARACTERS WITH FREQ>=k BCZ ALL OTHER WONT BE USEFUL;
+        // //This saves time further
+        // string tmp;
+        // for (auto ch: s){
+        //     if (candidates.find(ch)!=candidates.end()) tmp+=ch;
+        // }
+        // s=tmp;
+
+        string ans=string(1, *candidates.begin());  //// Initialize answer with lexicographically largest single char string
+        int maxSize=0;
+        while(!q.empty()){
+            string curr=q.front();
+            q.pop();
+            for (auto ch: candidates){
+                string tmp=curr+ch;
+                if (isValidSubsequence(tmp, s, k)){ //// Check if tmp*k is subsequence of s
+                    q.push(tmp);
+                    if (tmp.size()>maxSize){
+                        ans=tmp;
+                        maxSize=tmp.size();
+                    }
+                }
+            }
+        }
         return ans;
+    }
+    bool isValidSubsequence(string tmp, string s, int k){
+        //This fns return true if (tmp*k) is a valid subsequence of s
+        int i=0;
+        string subsequence;
+        for (int i=0; i<k; i++){
+            subsequence+=tmp;
+        }
+        int j=0;
+        for (int i=0; i<s.size(); i++){
+            if (s[i]==subsequence[j]) j++;
+            if (j==subsequence.size()) return true;
+        }
+        return false;
     }
 };
